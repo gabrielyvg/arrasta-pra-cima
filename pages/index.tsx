d6ea372c0faa30ed9e8c1ac5f06c25b8e38df7a7
gabrielyvg <gabrielyvg@outlook.com>
2023-07-24 19:58:48 -0300

    Criando paginas e componentes base

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+
+export default function HomePage() {
+    return (
+        <>
+            <Header></Header>
+            <h1>Home</h1>
+            <Footer></Footer>
+        </>
+    )
+}
