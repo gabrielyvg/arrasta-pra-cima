645c4a54c37a28b015e3ff313f15c1190bc96ed1
gabrielyvg <gabrielyvg@outlook.com>
2023-07-24 21:34:52 -0300

    Mudança na estrutura de pastas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
+import Footer from "../src/app/components/Footer"
+import Header from "../src/app/components/Header"
 
 export default function HomePage() {
     return (
