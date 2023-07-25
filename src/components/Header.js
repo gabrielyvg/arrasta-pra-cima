9523454bf8c96c067ce45f8301f6525ace42fa24
andrelsmelo <andrelucassmelo@gmail.com>
2023-07-25 10:17:03 -0300

    removido estrutura de projeto e inserido nova

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from '@/styles/Header.module.css';
+
+export default function Header() {
+  return (
+    <header>
+        Header
+      <nav>
+        <Link href={'/'}>Home</Link>
+        <Link href={'/aulas'}>Aulas</Link>
+      </nav>
+    </header>
+  );
+}
